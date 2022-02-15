4cd561da61c0577e05719b1cf242045217ca709b
Guilherme Coutinho <lgcb@ic.ufal.br>
2022-02-15 16:08:05 -0300

    Igualando arquivos com o curso

--- Cliente.js
+++ Cliente.js
@@ -1,4 +1,7 @@
 export class Cliente{
+  nome;
+  _cpf;
+
   get cpf(){
       return this._cpf;
   }
--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,55 +1,54 @@
-import {
-  Cliente
-} from "./Cliente.js";
+import { Cliente } from "./Cliente.js";
 
-export class ContaCorrente {
-  static numeroDeContas = 0;
-
-  // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-
-
-
-
-  set cliente(novoValor) {
-      if (novoValor instanceof Cliente) {
-          this._cliente = novoValor;
-      }
-  }
-
-  get cliente() {
-      return this._cliente;
-  }
-
-  get saldo() {
-      return this._saldo;
-  }
-
-  constructor(agencia, cliente) {
-      this.agencia = agencia;
-      this.cliente = cliente;
-      this._saldo = 0;
-      ContaCorrente.numeroDeContas += 1;
-  }
+export class ContaCorrente{
+    static numeroDeContas = 0;
+    agencia;
+    _cliente;
+   // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+   _saldo = 0;
+    
 
 
-  sacar(valor) {
-      if (this._saldo >= valor) {
-          this._saldo -= valor;
-          return valor;
-      }
-  }
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
 
-  depositar(valor) {
-      if (valor <= 100) {
-          return;
-      }
-      this._saldo += valor;
-  }
+    get cliente(){
+        return this._cliente;
+    }
 
-  tranferir(valor, conta) {
+    get saldo(){
+        return this._saldo;
+    }
 
-      const valorSacado = this.sacar(valor);
-      conta.depositar(valorSacado);
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
-  }
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0)
+        {
+            return;
+        } 
+        this._saldo += valor;           
+    }
+
+    tranferir(valor, conta){
+        
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+        
+    }
 }
